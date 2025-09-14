34854f2c95341afc6ba8566b6bb6296ea99b58cc
MONESHGOMO <moneshd16@gmail.com>
2025-09-14 11:24:43 +0530

    feat : new ci cd

---
+++ src/test/java/com/gomo/Github/action/GithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomo.Github.action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
