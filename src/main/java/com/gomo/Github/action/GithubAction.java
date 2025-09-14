e78a3c1270575588d9b00aab96af6de2edb4f54b
MONESHGOMO <moneshd16@gmail.com>
2025-09-14 12:10:06 +0530

    feat : docker credentials

--- src/main/java/com/gomo/Github/action/GithubAction.java
+++ src/main/java/com/gomo/Github/action/GithubAction.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GithubActionApplication {
+public class GithubAction{
 
 	public static void main(String[] args) {
-		SpringApplication.run(GithubActionApplication.class, args);
+		SpringApplication.run(GithubAction.class, args);
 	}
 
 }
